208456c1cf397996dd1b9d3c1e9694a4da14e71b
ido sarue <dodojoker67@gmail.com>
2021-08-25 14:45:27 +0300

    styled info

--- hotel_proj/hotel_proj/settings.py
+++ hotel_proj/hotel_proj/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'hotel_proj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Israel'
 
 USE_I18N = True
 
